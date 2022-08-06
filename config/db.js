d0f6e6e305a8fa705f06a723b79294ef499aa044
Rutuja Kadam <kadamrutuja103@gmail.com>
2022-08-07 01:01:05 +0530

    database connect, upload file endpoint

--- config/db.js
+++ config/db.js
@@ -1,18 +1,31 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 
-function connectDB(){
-    //Database connection
 
-    mongoose.connect(url, {useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true,useFindAndModify:true});
+function connectDB(){
+
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+        useNewUrlParser: true, 
+        useUnifiedTopology:true
+    });
 
     const connection = mongoose.connection;
 
-    connection.once('open', ()=>{
-        console.log("Database connected.")
-    }).catch(err=>{
-        console.log("Connection failed.")
-    })
+    connection.once('open', () => {
+        console.log("DB connected successfully");
+    });
+    connection.on('error', () => {
+        console.log("DB connection failed");
+    });
 
-}
+ };
+
+
+
+
+
+
+
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv")
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
+dotenv.config();
+connectDB();
 
+
+//Routes
+
+app.use('/api/files', require('./routes/files'));
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`);
